e67f1cd51f1b573a4be21b2ffa2da695334da002
Manuela Mindroc <manuela.mindroc@earthlink.ro>
2019-09-06 16:04:26 +0300

    temporarily comments routes

--- src/routes/api.php
+++ src/routes/api.php
@@ -4,21 +4,19 @@ Route::middleware(['web', 'auth', 'core'])
         Route::namespace('LaravelEnso\Emails\app\Http\Controllers\Emails')
             ->prefix('api/emails')->as('emails.')
             ->group(function () {
-                
-		Route::get('', 'Index')->name('index');
-		Route::get('create', 'Create')->name('create');
-		Route::post('', 'Store')->name('store');
-		Route::get('{email}/edit', 'Edit')->name('edit');
+                // Route::get('', 'Index')->name('index');
+        // Route::get('create', 'Create')->name('create');
+        // Route::post('', 'Store')->name('store');
+        // Route::get('{email}/edit', 'Edit')->name('edit');
 
-		Route::patch('{email}', 'Update')->name('update');
+        // Route::patch('{email}', 'Update')->name('update');
 
-		Route::delete('{email}', 'Destroy')->name('destroy');
+        // Route::delete('{email}', 'Destroy')->name('destroy');
 
-		Route::get('initTable', 'InitTable')->name('initTable');
-		Route::get('tableData', 'TableData')->name('tableData');
-		Route::get('exportExcel', 'ExportExcel')->name('exportExcel');
+        // Route::get('initTable', 'InitTable')->name('initTable');
+        // Route::get('tableData', 'TableData')->name('tableData');
+        // Route::get('exportExcel', 'ExportExcel')->name('exportExcel');
 
-		Route::get('options', 'Options')->name('options');
-
-        });
+        // Route::get('options', 'Options')->name('options');
+            });
     });
