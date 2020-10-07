109780b745ce079ff28e0e43ad5db7432ae88309
Mihai <mihai.ocneanu@gmail.com>
2020-10-07 17:08:53 +0300

    updates routes

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use LaravelEnso\Emails\Http\Controllers\Emails\Send;
+use LaravelEnso\Emails\Http\Controllers\Emails\Store;
+use LaravelEnso\Emails\Http\Controllers\Emails\Edit;
+use LaravelEnso\Emails\Http\Controllers\Emails\Update;
+use LaravelEnso\Emails\Http\Controllers\Emails\Destroy;
+use LaravelEnso\Emails\Http\Controllers\Emails\InitTable;
+use LaravelEnso\Emails\Http\Controllers\Emails\TableData;
 
 Route::middleware(['api', 'auth', 'core'])
-    ->namespace('LaravelEnso\Emails\Http\Controllers\Emails')
     ->prefix('api/emails')
     ->as('emails.')
     ->group(function () {
-        Route::post('send', 'Send')->name('send');
-        Route::post('', 'Store')->name('store');
-        Route::get('{email}/edit', 'Edit')->name('edit');
-        Route::post('{email}/update', 'Update')->name('update');
-        Route::delete('{email}', 'Destroy')->name('destroy');
-        Route::get('initTable', 'InitTable')->name('initTable');
-        Route::get('tableData', 'TableData')->name('tableData');
+        Route::post('send', Send::class)->name('send');
+        Route::post('', Store::class)->name('store');
+        Route::get('{email}/edit', Edit::class)->name('edit');
+        Route::post('{email}/update', Update::class)->name('update');
+        Route::delete('{email}', Destroy::class)->name('destroy');
+        Route::get('initTable', InitTable::class)->name('initTable');
+        Route::get('tableData', TableData::class)->name('tableData');
     });
